f81c93615fadfe83be7a1eb212b043949354db83
armiller <armiller@osuosl.org>
2013-12-18 17:41:56 -0800

    Default user etherpad

--- attributes/default.rb
+++ attributes/default.rb
@@ -34,9 +34,7 @@ default['etherpad-lite']['admin_enabled'] = false
 default['etherpad-lite']['admin_password'] = ""
 default['etherpad-lite']['log_level'] = "INFO"
 
-service_user = "etherpad-user"
-
-default['etherpad-lite']['service_user'] = service_user
+default['etherpad-lite']['service_user'] = "etherpad"
 default['etherpad-lite']['service_user_gid'] = 500
 default['etherpad-lite']['service_user_home'] = "/home/#{service_user}"
 default['etherpad-lite']['service_name'] = "etherpad"
